049e22668655f0409cd73903ce5ec42ac204e572
Aarif Mohammad <aarifmohammad9001@gmail.com>
2023-02-11 22:34:31 +0530

    new file add

---
+++ index.js
@@ -0,0 +1,17 @@
+// let m = 10
+// while (m>=1) {
+//   console.log(m);
+//   m--
+// }
+// let ans = Number(prompt("how many time you want to print hello word"))
+
+// let m = ans
+// do {
+// console.log("hello world");
+// m++
+
+// } while (m<=5)
+
+
+// 1
+
